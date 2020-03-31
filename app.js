dc9586a2f2738db3933dc0e37c5832354e1b98d5
Stavros Vergos <stavrospvergos@gmail.com>
2020-03-31 17:12:14 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit',calculateResults);
+
+// Calculate results
+function calculateResults(e){
+    
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) /100 /12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payment
+
+    e.preventDefault();
+}
